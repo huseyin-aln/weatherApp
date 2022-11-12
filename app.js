87a6a9f5f3bdadca6c5189799bd8f73965465252
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-11-12 10:26:54 +0300

    added apiKey

--- app.js
+++ app.js
@@ -3,43 +3,50 @@ const input = document.querySelector(".top-banner input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
 
+localStorage.setItem(
+  "apiKey",
+  EncryptStringAES("4d8fb5b93d4af21d66a2948710284366")
+);
 
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    getWeatherDataFromApi();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeatherDataFromApi();
 });
 
-const getWeatherDataFromApi = async() =>{
-    let tokenKey = DecryptStringAES(localStorage.getItem("apiKey"));
+const getWeatherDataFromApi = async () => {
+  let tokenKey = DecryptStringAES(localStorage.getItem("apiKey"));
 
-    let inputVal = input.value;
-    let unitType = "metric";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${tokenKey}&units=${unitType}`;
+  let inputVal = input.value;
+  let unitType = "metric";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${tokenKey}&units=${unitType}`;
 
-    try {
-        const response = await axios(url);
-        const { name, main, sys, weather } = response.data;
-        // console.log(response.data);
-        let iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+  try {
+    const response = await axios(url);
+    const { name, main, sys, weather } = response.data;
+    // console.log(response.data);
+    let iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
 
-        const cityListItem = list.querySelectorAll(".city");
-        const cityListItemsArray = Array.from(cityListItem);
-        if (cityListItemsArray.length > 0) {
-            const filteredArray = cityListItemsArray.filter(cityCard => cityCard.querySelector("span").innerText == name);
-            if (filteredArray.length > 0) {
-                msg.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
-                setTimeout(() => {
-                    msg.innerText = "";
-                }, 5000);
-                form.reset();
-                return;
-            }
-        }
+    const cityListItem = list.querySelectorAll(".city");
+    const cityListItemsArray = Array.from(cityListItem);
+    if (cityListItemsArray.length > 0) {
+      const filteredArray = cityListItemsArray.filter(
+        (cityCard) => cityCard.querySelector("span").innerText == name
+      );
+      if (filteredArray.length > 0) {
+        msg.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
+        setTimeout(() => {
+          msg.innerText = "";
+        }, 5000);
+        form.reset();
+        return;
+      }
+    }
 
-        const createdLi = document.createElement("li");
-        createdLi.classList.add("city");
-        const createdLiInnerHTML = 
-            `<h2 class="city-name" data-name="${name}, ${sys.country}">
+    const createdLi = document.createElement("li");
+    createdLi.classList.add("city");
+    const createdLiInnerHTML = `<h2 class="city-name" data-name="${name}, ${
+      sys.country
+    }">
                 <span>${name}</span>
                 <sup>${sys.country}</sup>
             </h2>
@@ -48,19 +55,16 @@ const getWeatherDataFromApi = async() =>{
                 <img class="city-icon" src="${iconUrl}">
                 <figcaption>${weather[0].description}</figcaption>
             </figure>`;
-        createdLi.innerHTML = createdLiInnerHTML;
+    createdLi.innerHTML = createdLiInnerHTML;
 
-        list.prepend(createdLi);
-
-    } catch (error) {
-        msg.innerText = error;
-        setTimeout(() => {
-            msg.innerText = "";
-        }, 5000);
-        form.reset();
-    }
-
+    list.prepend(createdLi);
+  } catch (error) {
+    msg.innerText = error;
+    setTimeout(() => {
+      msg.innerText = "";
+    }, 5000);
     form.reset();
-}
+  }
 
-
+  form.reset();
+};
